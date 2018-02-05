8e4d8516edecd77be298c73767e58ee58d1769c8
Abhijeet Dhupia <abhijeetdhoopia1997@gmail.com>
2018-02-06 00:00:54 +0530

    Inverse Cached Matrix
    Solution of the assignment question.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to cache the inverse of a matix 
 
-## Write a short comment describing this function
+## Below function will create a special matrix object which can cache it's inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        ## Assigning initial values 
+        i <- NULL 
+        
+        ## Set the matrix 
+        set <- function(y) {
+                x <<- y
+                i <<- NULL 
+        }
+        
+        ## Get the matrix 
+        get <- function() x
+        
+        ##Set the inverse of the entered matrix
+        setinverse <- function(inverse) i <<- inverse
+       
+        ## Get the inverse of the entered matrix 
+         getinverse <- function() i
+         
+        ## Return all the values
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The below function checks for the cache and if it is there then it shows the result and skips the compututation part.
+## If not, then it computes the inverse and store the the value in the cache. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+       
+         i <- x$getinverse()
+        if (!is.null(i)){
+                message("Collecting cached data ")
+                return(i)
+        }
+        m <- x$get()
+        i <- solve(m,...)
+        x$setinverse(i)
+        i 
 }
+
+
+#Sample input and output:
+#Input matrix 'a'
+
+# a <- matrix(c(2, 1, 3, 5), 2, 2) 
+# b <- makeCacheMatrix(a)
+# b$get()
+
+# [,1] [,2]                     ;(the output is the entered matrix) 
+# [1,]    2    3
+# [2,]    1    5
+
+#   cacheSolve(b)               ;(inverse of the entred matrix)
+# [,1]       [,2]
+# [1,]  0.7142857 -0.4285714
+# [2,] -0.1428571  0.2857143
+
